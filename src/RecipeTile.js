0a97b81976f3bedfbe4908e4db2eff6fb099c9da
Bhavika Chauhan <63841109+bhavika15-star@users.noreply.github.com>
2021-09-15 10:35:13 +0530

    Update RecipeTile.js

--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -4,7 +4,9 @@ export default function RecipeTile({recipe}) {
     return (
         recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/)
         !=null &&(
-        <div className="recipeTile">
+        <div className="recipeTile" onClick={()=>{
+            window.open(recipe["recipe"]["url"]);
+        }}>>
             <img className="recipeTile_img" src={recipe["recipe"]["image"]}/>
             <p className="recipeTile_name">{recipe["recipe"]["label"]}</p> 
         </div>
